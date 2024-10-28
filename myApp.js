11cf1eded0a8cbd8df363352692842210b85a098
Adam Berrou <adazbebiz@gmail.com>
2024-10-28 11:13:48 +0000

    ChangedPositionOfPersonInstantiation

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ const Person = PersonSchema( 'Person', PersonSchema);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const PersonSchema = new mongoose.Schema({
-  name: {type: String, required: true}
+  name: {type: String, required: true},
   age :{type: Number},
   favoriteFoods: [String] 
 });
