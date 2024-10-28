a804fed0b5f2dc8504d10b30ebed5e7492492008
Adam Berrou <adazbebiz@gmail.com>
2024-10-28 09:27:12 +0000

    PersonSchema

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
 
+let mongoose =require('mongoose');
 
-let Person;
+
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Person = new mongoose.Schema({
+  name: { type: String, required: true },
+  age :{type: Number},
+  favoriteFoods: [String] 
+});
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
