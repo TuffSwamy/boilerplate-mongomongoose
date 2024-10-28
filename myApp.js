97fa326bea74cae57d7b474bc2db013e52f71c53
Adam Berrou <adazbebiz@gmail.com>
2024-10-28 11:52:01 +0000

    modelAdd

--- myApp.js
+++ myApp.js
@@ -4,7 +4,6 @@ let mongoose =require('mongoose');
 const Schema = mongoose.Schema;
 
 
-const Person = PersonSchema( 'Person', PersonSchema);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -13,6 +12,7 @@ const PersonSchema = new mongoose.Schema({
   age :{type: Number},
   favoriteFoods: [String] 
 });
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
